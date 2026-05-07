512e8fdfe2085037ef5c99ff793700f409a9ecea
contatomvabf <contatomvabf@gmail.com>
2026-05-07 08:27:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -772,8 +772,16 @@
         submitBtn.textContent = "ENVIANDO...";
       }
 
+      function resetBtn() {
+        form.setAttribute("data-submitting", "0");
+        if (submitBtn) {
+          submitBtn.disabled = false;
+          submitBtn.textContent = "ENVIAR";
+        }
+      }
+
       grecaptcha.ready(function () {
-        grecaptcha.execute("6Ldi7N0sAAAAAB4-reoB5hlDNb6mCue37xQJSMlF", { action: "submit" })
+        grecaptcha.execute("6Ldi7N0sAAAAB4-reoBShIDNb6mCue37xQJSMIF", { action: "submit" })
         .then(function (token) {
           return fetch("https://formspree.io/f/xdabdzqa", {
             method: "POST",
@@ -796,16 +804,11 @@
         .then(function () {
           setFormFeedback("success", "Mensagem enviada com sucesso! Em breve entraremos em contato.");
           form.reset();
+          resetBtn();
         })
         .catch(function () {
           setFormFeedback("error", "Não foi possível enviar agora. Tente novamente em instantes.");
-        })
-        .finally(function () {
-          form.setAttribute("data-submitting", "0");
-          if (submitBtn) {
-            submitBtn.disabled = false;
-            submitBtn.textContent = "ENVIAR";
-          }
+          resetBtn();
         });
       });
     });
